a99b8739a730e4be028412b9aea9374ecbb3b3ed
kadirnurkovic <kadirnurkovic0404@gmail.com>
2022-12-27 21:19:11 +0100

    ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,31 @@
-import React, { FC } from "react";
+import React, { FC, ChangeEvent, useState } from "react";
 import "./App.css";
+import {InterfaceTask} from "./components/interfaces"
 
 const App: FC = () => {
+
+  const [task, setTask] = useState<string>("");
+  const [todo, setTodo] = useState<InterfaceTask[]>([]);
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>): void => {
+    if(event.target.name === "task"){
+       setTask(event.target.value)
+    }
+  }
+
+  const addTask = (): void => {
+      const newTask = { taskName: task };
+      setTodo([...todo, newTask]);
+      console.log(todo);
+  }
+
   return (
     <div className="App">
       <div className="header">
         <div className="inputContainer">
-          <input type="text" placeholder="Task..." />
+          <input type="text" placeholder="Task..." name="task" onChange={handleChange}/>
+          <button className="buttonForAddingTask" onClick={addTask}>Add task</button>
         </div>
-        <button>Add task</button>
         <div className="todo"></div>
       </div>
     </div>
---
+++ src/components/interfaces.ts
@@ -0,0 +1,3 @@
+export interface InterfaceTask {
+    taskName: string;
+}
